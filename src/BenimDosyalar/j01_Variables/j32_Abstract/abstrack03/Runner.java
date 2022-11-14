b9355c78dec8baa1bc911d348f4521d84a274f5b
ismail5240 <arkd50279@gmail.com>
2022-11-15 00:04:46 +0300

    15.11.22

--- src/BenimDosyalar/j01_Variables/j32_Abstract/abstrack03/Runner.java
+++ src/BenimDosyalar/j01_Variables/j32_Abstract/abstrack03/Runner.java
@@ -3,6 +3,17 @@ package BenimDosyalar.j01_Variables.j32_Abstract.abstrack03;
 public class Runner {
     public static void main(String[] args) {
 
+        Dikdörtgen dd = new Dikdörtgen(24,7);
+        dd.setName("dikdörtgen ");
+        // dd.cevreHesapla();//
+        // dd.alanHesapla();//
+        // System.out.println("dd.ciz() = " + dd.ciz());//
+        System.out.println(dd);//
+
+        Cember c1=new Cember(39);
+        c1.setName("cemberimde gül oya ");
+        System.out.println(c1);
 
     }
-}
+    }
+
