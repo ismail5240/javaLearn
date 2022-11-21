963deafac7f0427198feb5545c5d27bb548d79eb
ismail5240 <arkd50279@gmail.com>
2022-11-21 22:11:05 +0300

    21/11/22

---
+++ src/BenimDosyalar/j01_Variables/j35_Collection/Task/Task_1.java
@@ -0,0 +1,38 @@
+package BenimDosyalar.j01_Variables.j35_Collection.Task;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Task_1 {
+    public static void main(String[] args) {
+/*
+    Task
+    Parametre olarak bir String HashSet   ve  iki String alan  return type HashSet
+   olan  changeSet() isminde bir method oluşturun.
+    Eğer HashSet String 1 e sahipse,HashSet'in  String 1 elemanını String 2 ile değiştirin.
+    ÖRNEK:
+    hashset = "banana" , "strawberry" , "kiwi" , "pineapple"
+    String 1 = banana
+    String 2 = peach
+    CEVAP: "peach" , "strawberry" , "kiwi" , "pineapple"
+     */
+
+
+        HashSet<String> hs= new HashSet<>(Arrays.asList("Adem" ,"Ismail","Sumeyra","Murat","Busra"));
+        String str1="Adem";
+        String str2="Umit";
+
+        changeSet(hs,str1,str2);
+
+        System.out.println("hs = " + hs);
+    }
+    private static HashSet changeSet(HashSet<String>hs,String str1,String str2) {
+
+        if (hs.contains(str1)){
+            hs.remove(str1);
+            hs.add(str2);
+        }
+        return hs;
+
+    }
+}
---
+++ src/BenimDosyalar/j01_Variables/j36_Map/C.java
@@ -0,0 +1,4 @@
+package BenimDosyalar.j01_Variables.j36_Map;
+
+public class C {
+}
